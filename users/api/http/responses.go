e899e9cbad00e643e69b23b1a3c617e203c1b0ed
Mirko Teodorovic <mirko.teodorovic@gmail.com>
2019-09-21 07:23:46 +0200

    fix identityRes
    Signed-off-by: Mirko Teodorovic <mirko.teodorovic@gmail.com>

--- users/api/http/responses.go
+++ users/api/http/responses.go
@@ -13,15 +13,13 @@ import (
 	"github.com/mainflux/mainflux"
 )
 
-var _ mainflux.Response = (*tokenRes)(nil)
+var (
+	_ mainflux.Response = (*tokenRes)(nil)
+	_ mainflux.Response = (*identityRes)(nil)
+)
 
 type tokenRes struct {
 	Token string `json:"token,omitempty"`
-}
-
-type identityRes struct {
-	Email    string                 `json:"email"`
-	Metadata map[string]interface{} `json:"metadata,omitempty"`
 }
 
 func (res tokenRes) Code() int {
@@ -34,4 +32,21 @@ func (res tokenRes) Headers() map[string]string {
 
 func (res tokenRes) Empty() bool {
 	return res.Token == ""
+}
+
+type identityRes struct {
+	Email    string                 `json:"email"`
+	Metadata map[string]interface{} `json:"metadata,omitempty"`
+}
+
+func (res identityRes) Code() int {
+	return http.StatusCreated
+}
+
+func (res identityRes) Headers() map[string]string {
+	return map[string]string{}
+}
+
+func (res identityRes) Empty() bool {
+	return res.Email == "" && len(res.Metadata) == 0
 }
