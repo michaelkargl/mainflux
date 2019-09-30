d2eb16234285705beb539c02ae17dbecfb61d179
Mirko Teodorovic <mirko.teodorovic@gmail.com>
2019-09-30 14:35:03 +0200

    add token methods
    Signed-off-by: Mirko Teodorovic <mirko.teodorovic@gmail.com>

--- users/postgres/users.go
+++ users/postgres/users.go
@@ -61,26 +61,6 @@ func (ur userRepository) RetrieveByID(ctx context.Context, email string) (users.
 	return user, nil
 }
 
-<<<<<<< HEAD
-// dbMetadata type for handling metadata properly in database/sql
-type dbMetadata map[string]interface{}
-
-// Scan - Implement the database/sql scanner interface
-func (m *dbMetadata) Scan(value interface{}) error {
-	if value == nil {
-		m = nil
-		return nil
-	}
-
-	b, ok := value.([]byte)
-	if !ok {
-		m = &dbMetadata{}
-		return users.ErrScanMetadata
-	}
-
-	if err := json.Unmarshal(b, m); err != nil {
-		m = &dbMetadata{}
-=======
 func (ur userRepository) SaveToken(_ context.Context, email, token string) error {
 	t, err := ur.retrieveTokenByID(email)
 	if err != nil {
@@ -103,42 +83,23 @@ func (ur userRepository) SaveToken(_ context.Context, email, token string) error
 		if pqErr, ok := err.(*pq.Error); ok && errDuplicate == pqErr.Code.Name() {
 			return users.ErrConflict
 		}
->>>>>>> add token
-		return err
 	}
 
 	return nil
 }
 
-<<<<<<< HEAD
-// Value Implements valuer
-func (m dbMetadata) Value() (driver.Value, error) {
-	if len(m) == 0 {
-		return nil, nil
-	}
-
-	b, err := json.Marshal(m)
-	if err != nil {
-		return nil, err
-	}
-	return b, err
-=======
 func (ur userRepository) RetrieveToken(_ context.Context, email string) (string, error) {
 	return ur.retrieveTokenByID(email)
 }
 
-func (ur userRepository) DeleteToken(email string) (string, error) {
-	q := `SELECT token from tokens WHERE email = $1`
+func (ur userRepository) DeleteToken(_ context.Context, email string) error {
+	q := `DELETE  FROM tokens WHERE email = $1`
 
-	t := ""
-	if err := ur.db.QueryRowx(q, email).StructScan(&t); err != nil {
-		if err == sql.ErrNoRows {
-			return t, users.ErrNotFound
-		}
-		return t, err
+	if _, err := ur.db.NamedExec(q, email); err != nil {
+		return err
 	}
 
-	return t, nil
+	return nil
 }
 
 func (ur userRepository) retrieveTokenByID(email string) (string, error) {
@@ -153,42 +114,63 @@ func (ur userRepository) retrieveTokenByID(email string) (string, error) {
 	}
 
 	return t, nil
->>>>>>> add token
+}
+
+// dbMetadata type for handling metadata properly in database/sql
+type dbMetadata map[string]interface{}
+
+// Scan - Implement the database/sql scanner interface
+func (m *dbMetadata) Scan(value interface{}) error {
+	if value == nil {
+		m = nil
+		return nil
+	}
+
+	b, ok := value.([]byte)
+	if !ok {
+		m = &dbMetadata{}
+		return users.ErrScanMetadata
+	}
+
+	if err := json.Unmarshal(b, m); err != nil {
+		m = &dbMetadata{}
+		return err
+	}
+
+	return nil
+}
+
+// Value Implements valuer
+func (m dbMetadata) Value() (driver.Value, error) {
+	if len(m) == 0 {
+		return nil, nil
+	}
+
+	b, err := json.Marshal(m)
+	if err != nil {
+		return nil, err
+	}
+	return b, err
 }
 
 type dbUser struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Metadata []byte `json:"metadata,omitempty"`
+	Email    string     `db:"email"`
+	Password string     `db:"password"`
+	Metadata dbMetadata `db:"metadata"`
 }
 
-func toDBUser(u users.User) (dbUser, error) {
-	m := []byte("{}")
-	if len(u.Metadata) > 0 {
-		data, err := json.Marshal(u.Metadata)
-		if err != nil {
-			return dbUser{}, err
-		}
-		m = data
-	}
-
+func toDBUser(u users.User) dbUser {
 	return dbUser{
 		Email:    u.Email,
 		Password: u.Password,
-		Metadata: m,
-	}, nil
+		Metadata: u.Metadata,
+	}
 }
 
 func toUser(dbu dbUser) users.User {
-
-	var metadata map[string]interface{}
-	if err := json.Unmarshal([]byte(dbu.Metadata), &metadata); err != nil {
-		return users.User{}
-	}
-
 	return users.User{
 		Email:    dbu.Email,
 		Password: dbu.Password,
-		Metadata: metadata,
+		Metadata: dbu.Metadata,
 	}
 }
